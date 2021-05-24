04d0829ea16026b491d0966ea0fda6cbc1c51151
DarekOkninski <d.okninski98@gmail.com>
2021-05-24 13:44:49 +0200

    start

---
+++ src/main/java/pl/stadler/counter/services/IsolationsCableService.java
@@ -0,0 +1,31 @@
+package pl.stadler.counter.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.stadler.counter.models.IsolationsCable;
+import pl.stadler.counter.repositories.IsolationsCableRepository;
+
+import java.util.List;
+
+@Service
+public class IsolationsCableService {
+
+
+    private final IsolationsCableRepository isolationsCableRepository;
+
+    @Autowired
+    public IsolationsCableService(IsolationsCableRepository isolationsCableRepository){
+        this.isolationsCableRepository=isolationsCableRepository;
+    }
+
+    public List<IsolationsCable> findAll() {
+        return isolationsCableRepository.findAll();
+    }
+    public IsolationsCable findByTypeIsolationsAndPrzekrojWew(String typeIsolations, String przekrojWew) {
+        return isolationsCableRepository.findByTypeIsolationsAndPrzekrojWew(typeIsolations, przekrojWew).orElse(null);
+    }
+    public IsolationsCable save(IsolationsCable isolationsCable){
+        return isolationsCableRepository.save(isolationsCable);
+    }
+
+}
